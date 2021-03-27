c76bb69c0533f202f28d1825e02e156a23781fc9
Osman Ali <cs.osman@outlook.com>
2021-03-27 21:30:11 +0300

    Show the score in top of page

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const correctAnswers = ['B', 'B', 'B', 'B'];
 const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
@@ -9,11 +10,14 @@ form.addEventListener('submit', e => {
     const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
 
     //check Anwers
-
     userAnswers.forEach((answer, index) => {
         if(answer === correctAnswers[index]){
             score += 25;
         }
     });
-    console.log(score);
+
+    //show result on page
+    result.querySelector('span').textContent = `${score}%`;
+    result.classList.remove('d-none');
+
 });
